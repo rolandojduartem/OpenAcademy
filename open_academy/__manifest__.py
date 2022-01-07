df37c0dfa07de241a8ca222c1dd2ffc54a951c13
rolandojduartem <rolando@vauxoo.com>
2022-01-07 10:41:06 -0600

    [IMP] open_academy: Add data demo T#52898
    [IMP] open_academy: Add data demo T#52898

--- open_academy/__manifest__.py
+++ open_academy/__manifest__.py
@@ -13,8 +13,9 @@
     'version': '15.0.1.0.0',
     'depends': ['base'],
     'data': [
-        'security/ir.model.access.csv'
+        'security/ir.model.access.csv',
     ],
     'demo': [
+        'demo/course.xml',
     ],
 }
