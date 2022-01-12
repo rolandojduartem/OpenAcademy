5bb3e3fd2f093bfb27c7a658814acfbeb35ee19a
rolandojduartem <rolando@vauxoo.com>
2022-01-11 18:59:05 -0600

    [IMP] open_academy: Include a session_ids o2m relation on course model T#52898

--- open_academy/models/course.py
+++ open_academy/models/course.py
@@ -4,6 +4,7 @@ from odoo import models, fields
 class Course(models.Model):
     _name = 'course'
     _description = 'Course'
+    session_ids = fields.One2many('session', 'course_id')
     responsible_id = fields.Many2one('res.users')
     title = fields.Char(required=True)
     description = fields.Text()
