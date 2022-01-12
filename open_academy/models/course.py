3b38acd1bf7b289a89920c7b16582d0c0908a8cc
rolandojduartem <rolando@vauxoo.com>
2022-01-11 18:29:17 -0600

    [IMP] open_academy: Include relation between course with responsible and sessions with course and instructor T#52898

--- open_academy/models/course.py
+++ open_academy/models/course.py
@@ -4,5 +4,6 @@ from odoo import models, fields
 class Course(models.Model):
     _name = 'course'
     _description = 'Course'
+    responsible_id = fields.Many2one('res.users')
     title = fields.Char(required=True)
     description = fields.Text()
--- open_academy/models/session.py
+++ open_academy/models/session.py
@@ -4,6 +4,8 @@ from odoo import models, fields
 class Session(models.Model):
     _name = 'session'
     _description = 'Session'
+    course_id = fields.Many2one('course', required=True)
+    instructor_id = fields.Many2one('res.partner')
     name = fields.Char(required=True)
     start_date = fields.Date()
     duration = fields.Float()
