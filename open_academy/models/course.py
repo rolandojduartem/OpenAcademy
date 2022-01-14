0be145b8ac4a7fc7d6f8565314bd69b9cc3942e7
rolandojduartem <rolando@vauxoo.com>
2022-01-14 13:54:26 -0600

    [IMP] open_academy: Add some SQL constraints to state that course title will be UNIQUE and title can not be equal to description T#52898

--- open_academy/models/course.py
+++ open_academy/models/course.py
@@ -4,6 +4,10 @@ from odoo import models, fields
 class Course(models.Model):
     _name = 'course'
     _description = 'Course'
+    _sql_constraints = [
+        ('check_title_description', 'CHECK(title<>description)', "The course's title can not be equal to course's description"),
+        ('title_unique', 'UNIQUE(title)', "The course's title exists"),
+    ]
     session_ids = fields.One2many('session', 'course_id')
     responsible_id = fields.Many2one('res.users')
     title = fields.Char(required=True)
