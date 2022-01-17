69d850545ac4aa1d462bc6a7f7a66587d4f83c80
rolandojduartem <rolando@vauxoo.com>
2022-01-17 16:46:36 -0600

    [IMP] open_academy: Modify the copy method to handle the case when the user try to save a course with a duplicated title T#52898

--- open_academy/models/course.py
+++ open_academy/models/course.py
@@ -3,6 +3,7 @@ from odoo import models, fields
 
 class Course(models.Model):
     _name = 'course'
+    _rec_name = 'title'
     _description = 'Course'
     _sql_constraints = [
         ('check_title_description', 'CHECK(title<>description)', "Title can not be equal to description"),
@@ -10,5 +11,11 @@ class Course(models.Model):
     ]
     session_ids = fields.One2many('session', 'course_id')
     responsible_id = fields.Many2one('res.users')
-    title = fields.Char(required=True)
+    title = fields.Char(required=True, copy=False)
     description = fields.Text()
+
+    def copy_data(self, default=None):
+        res = super().copy_data(default=default)
+        for copy_values in res:
+            copy_values['title'] = 'Copy of [%s]' % self.title
+        return res
