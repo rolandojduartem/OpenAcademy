dcaaee099f69c2acabf34d8d107e1dc5294d6cac
rolandojduartem <rolando@vauxoo.com>
2022-01-20 18:08:08 -0600

    [IMP] open_academy: Register attendees. I added a button with the function to save the attendee to the chosen session T#52898

--- open_academy/wizards/assign_attendee_sessions.py
+++ open_academy/wizards/assign_attendee_sessions.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, Command
 
 
 class AssignAttendeeSessions(models.TransientModel):
@@ -6,3 +6,6 @@ class AssignAttendeeSessions(models.TransientModel):
     _description = 'Assign attendees'
     session_id = fields.Many2one('session', required=True)
     attendee_ids = fields.Many2many('res.partner')
+
+    def save_this_session(self):
+        self.session_id.write({'attendee_ids': [Command.link(attendee.id) for attendee in self.attendee_ids]})
